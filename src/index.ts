2654fff14ced002487cd3a6656571a346c4420d1
marcus.salinas <12.marcus.salinas@gmail.com>
2025-12-11 00:10:02 -0600

    feat: Initialize TypeScript project with build configuration, dependencies, and a basic module.

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * W1 Pending Data Parser
+ * 
+ * Takes 11 files from the Texas RRC Drilling Permit data dump and converts it to useable classes and JSON.
+ */
+
+export function helloWorld(): string {
+    return "Hello from w1-pending-data-parser!";
+}
